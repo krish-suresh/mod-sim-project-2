04433bd5e87a57feaa70f7daa2d8a2830103650e
Krishna Suresh <krishna4132@gmail.com>
2021-11-03 16:03:52 -0400

    minor fix

--- paraboloid_radius_to_cost.m
+++ paraboloid_radius_to_cost.m
@@ -3,5 +3,5 @@ function res = paraboloid_radius_to_cost(r, h, cost_per_square_m)
     function res = func(x)
         res = 2*pi*sqrt(1+(a*x).^2).*x;
     end
-    res = integral(@func, 0, r)*2*cost_per_square_m;
+    res = integral(@func, 0, r)*cost_per_square_m;
 end
